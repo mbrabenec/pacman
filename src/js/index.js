9d10cc6daf0f57f9a656a471df50ea68adba6e2e
snelling-a <snelling.andrew@gmail.com>
2021-01-29 10:19:54 +0100

    wokka wokka

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,8 @@
 'use strict';
 
-console.log('it works!');
+document.addEventListener('keydown', (event) => {
+  const pacEntity = document.querySelector('.entity--pac');
+  pacEntity.classList.toggle('closed');
+  // if (event.code === 'ArrowRight') {
+
+});
