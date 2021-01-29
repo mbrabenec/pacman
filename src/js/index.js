a700ac2a6cffc22bfaa4db2a541fb98e5cbd64c7
snelling-a <snelling.andrew@gmail.com>
2021-01-29 10:27:38 +0100

    event listeners

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,17 @@
 document.addEventListener('keydown', (event) => {
   const pacEntity = document.querySelector('.entity--pac');
   pacEntity.classList.toggle('closed');
-  // if (event.code === 'ArrowRight') {
+  if (event.code === 'ArrowRight') {
+    pacEntity.class = 'right';
+  }
+  if (event.code === 'ArrowLeft') {
+    pacEntity.class = 'left';
+  }
+  if (event.code === 'ArrowUp') {
+    pacEntity.classList.toggle('up');
+  }
+  if (event.code === 'ArrowDown') {
+    pacEntity.classList.toggle('down');
+  }
 
 });
