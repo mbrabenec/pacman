d4d303bb7565dfb5d99af54da80ede5da4d3c6a6
snelling-a <snelling.andrew@gmail.com>
2021-01-29 10:32:54 +0100

    wokka up down left right

--- src/js/index.js
+++ src/js/index.js
@@ -4,16 +4,22 @@ document.addEventListener('keydown', (event) => {
   const pacEntity = document.querySelector('.entity--pac');
   pacEntity.classList.toggle('closed');
   if (event.code === 'ArrowRight') {
-    pacEntity.class = 'right';
+    pacEntity.classList.remove('left', 'up', 'down');
+    pacEntity.classList.add('right');
   }
   if (event.code === 'ArrowLeft') {
-    pacEntity.class = 'left';
+    pacEntity.classList.remove('right', 'up', 'down');
+    pacEntity.classList.add('left');
   }
   if (event.code === 'ArrowUp') {
-    pacEntity.classList.toggle('up');
+    pacEntity.classList.remove('left', 'left', 'down');
+
+    pacEntity.classList.add('up');
   }
   if (event.code === 'ArrowDown') {
-    pacEntity.classList.toggle('down');
+    pacEntity.classList.remove('left', 'up', 'right');
+
+    pacEntity.classList.add('down');
   }
 
 });
