44ca71a4dc22d3f0beb02ad7f4e27da2bc281d8a
snelling-a <snelling.andrew@gmail.com>
2021-01-29 13:12:29 +0100

    trying to render pacman

--- src/js/index.js
+++ src/js/index.js
@@ -1,60 +1,85 @@
 'use strict';
 const TILE_SIZE = 85;
-let xpos = 0;
+/*let xpos = 0;
+ */
+class PacStage {
+  constructor() {
+    this.stage = this.renderStage()
+  }
+  renderStage() {
+    const pacStage = document.createElement('div');
+    pacStage.className = "stage";
+    return pacStage;
+  }
+  renderElements() {
+    const pacman = new Pacman(this);
+  }
 
-
+  mount(parent) {
+    this.stageElement = this.renderStage();
+    parent.appendChild(this.stageElement);
+    return this.stageElement;
+  }
+}
 
 class Pacman {
-  constructor(xpos, ypos) {
-    this.xpos = xpos;
-    this.ypos = ypos
-    this.pacEntity = _pacEntity()
+  constructor(pacStage /* xpos, ypos */ ) {
+    /* this.xpos = xpos;
+    this.ypos = ypos */
+    this.pacStage = pacStage;
+    this.pacman = this._renderPacman();
+
+    this.pacStage.stage.appendChild(this.pacman)
   }
 
-  _pacEntity() {
+  _renderPacman() {
     const pacEntity = document.createElement('div');
-    pacEntity.class = 'entity--pac';
-    return pacEntity
-
+    pacEntity.className = ('entity', 'entity--pac', 'pacboy-active-light')
+    document.addEventListener('keydown', (e) => {
+      pacEntity.classList.toggle('closed');
+      move(e);
+    });
+    return pacEntity;
   }
-
-  render() {}
-  mount() {}
   update() {
-    this.pacEntity.style.left = `${this.xpos}px`;
-    this.pacEntity.style.top = `${this.ypos}px`;
+    this.pacEntity.style.left = `${this.xpos * TILE_SIZE}px`;
+    this.pacEntity.style.top = `${this.ypos * TILE_SIZE}px`;
   }
+  move(e) {
+    switch (e.key) {
+      case 'ArrowLeft':
+        this.xpos -= 1;
+        this.pacEntity.classList.remove('right', 'up', 'down');
+        this.pacEntity.classList.add('left');
+        this.update();
+        break;
+      case 'ArrowRight':
+        this.xpos += 1;
+        this.pacEntity.classList.remove('left', 'up', 'down');
+        this.pacEntity.classList.add('right');
+        this.update();
+        break;
+      case 'ArrowUp':
+        this.ypos -= 1;
+        this.pacEntity.classList.remove('right', 'left', 'down');
+        this.pacEntity.classList.add('up');
+        this.update();
+        break;
+      case 'ArrowDown':
+        this.ypos += 1;
+        this.pacEntity.classList.remove('right', 'left', 'up');
+        this.pacEntity.classList.add('down');
+        this.update();
+        break;
+      default:
+        console.log('stop mashing the keyboard')
 
-  moveRight() {
-    this.xpos += 85;
-    this.pacEntity.classList.remove('left', 'up', 'down');
-    this.pacEntity.classList.add('right');
-    this.update();
-  }
-
-  moveLeft() {
-    this.xpos -= 85;
-    this.pacEntity.classList.remove('right', 'up', 'down');
-    this.pacEntity.classList.add('left');
-    this.update();
-  }
-
-  moveUp() {
-    ypos -= 85;
-    this.pacEntity.classList.remove('right', 'left', 'down');
-    this.pacEntity.classList.add('up');
-    this.update();
-  }
-
-  moveDown() {
-    ypos += 85;
-    this.pacEntity.classList.remove('right', 'left', 'up');
-    this.pacEntity.classList.add('up');
-    this.update();
-  }
+    }
+  };
 };
-
-const pacman = new Pacman()
+const app = document.querySelector('#app')
+const pacstage = new PacStage;
+pacstage.mount(app)
 
 
 /* document.addEventListener('keydown', (event) => {
